d3e47845a235cfdfa7ddda9bb9e3c2d568d6ae55
juju170 <jujujuirah112@gmail.com>
2025-10-10 06:12:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+// Fungsi navigasi antar halaman
+const content = document.getElementById("content");
+const buttons = document.querySelectorAll(".nav-btn");
 
+buttons.forEach(btn => {
+  btn.addEventListener("click", async () => {
+    const page = btn.dataset.page;
+    const res = await fetch(`pages/${page}.html`);
+    const html = await res.text();
+    content.innerHTML = html;
+  });
+});
