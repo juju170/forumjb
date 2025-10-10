57d5db125f1d0e3d29ba61e40fabe17b7c204597
juju170 <jujujuirah112@gmail.com>
2025-10-10 15:21:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -334,7 +334,6 @@ document.querySelectorAll(".send-comment").forEach((btn) => {
     });
   }
 } // ⬅️ Tambahkan ini untuk menutup fungsi loadHomePage()
-}
 // ==============================
 // ➕ HALAMAN POST (UPLOAD & FIRESTORE)
 // ==============================
